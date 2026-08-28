7c538f07d1e16b54d84a3b7468195a08fc2576f6
Khang10072302 <93765340+Khang10072302@users.noreply.github.com>
2026-08-28 13:00:07 -0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,23 +1,21 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDTo2m1Hib2II0DeVvX4Q5eDXApoMqt5xw",
-    authDomain: "flashcard-f2998.firebaseapp.com",
-    projectId: "flashcard-f2998",
-    storageBucket: "flashcard-f2998.firebasestorage.app",
-    messagingSenderId: "319650668178",
-    appId: "1:319650668178:web:a6266132b5d84a507dd04b",
-    measurementId: "G-755346WJEL"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTo2m1Hib2II0DeVvX4Q5eDXApoMqt5xw",
+  authDomain: "flashcard-f2998.firebaseapp.com",
+  projectId: "flashcard-f2998",
+  storageBucket: "flashcard-f2998.firebasestorage.app",
+  messagingSenderId: "319650668178",
+  appId: "1:319650668178:web:a6266132b5d84a507dd04b",
+  measurementId: "G-755346WJEL"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
