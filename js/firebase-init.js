22a63f89a6640dc1412a12704c18e87a106007eb
Khang10072302 <93765340+Khang10072302@users.noreply.github.com>
2026-08-29 12:53:57 -0700

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,8 +1,8 @@
 // Khởi tạo Firebase (dùng bản CDN dạng module, không cần build tool)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import {
-  getAuth, onAuthStateChanged, signInWithEmailAndPassword,
-  createUserWithEmailAndPassword, signOut
+  getAuth, onAuthStateChanged, signOut,
+  sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import {
   getFirestore, collection, doc, addDoc, updateDoc, deleteDoc,
@@ -15,6 +15,7 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export {
-  onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut,
+  onAuthStateChanged, signOut,
+  sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink,
   collection, doc, addDoc, updateDoc, deleteDoc, onSnapshot, query, orderBy, serverTimestamp
 };
